eb9e67f42857f5a744c85b2a3e39d0b621fdb6a6
cliuxinxin <cliuxinxin@gmail.com>
2016-05-22 23:53:47 +0800

    nav opt

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,25 +1,25 @@
 
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
+{{--<nav class="navbar navbar-default navbar-fixed-top">--}}
+    {{--<div class="container">--}}
+        {{--<div class="navbar-header">--}}
             {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">--}}
                 {{--<span class="sr-only">Toggle navigation</span>--}}
                 {{--<span class="icon-bar"></span>--}}
                 {{--<span class="icon-bar"></span>--}}
                 {{--<span class="icon-bar"></span>--}}
             {{--</button>--}}
-            <a class="navbar-brand" href="{{ url('/') }}">好邻居</a>
+            {{--<a class="navbar-brand" href="{{ url('/') }}">好邻居</a>--}}
 
-            @if(Auth::check())
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/timelines').'/'.Auth::user()->id }}">我的故事</a></li>
-                    <li><a href="{{ url('/points/user') }}">我的积分</a></li>
-                    <li><a href="{{ url('/usertasks') }}">我的求助</a></li>
-                    <li><a href="{{ url('/receivetasks') }}">我的帮忙</a></li>
-                    <li><a href="{{ url('/profile'.'/'.Auth::user()->id) }}">我的档案</a></li>
-                </ul>
-            @endif
-        </div>
+            {{--@if(Auth::check())--}}
+                {{--<ul class="nav navbar-nav">--}}
+                    {{--<li><a href="{{ url('/timelines').'/'.Auth::user()->id }}">我的故事</a></li>--}}
+                    {{--<li><a href="{{ url('/points/user') }}">我的积分</a></li>--}}
+                    {{--<li><a href="{{ url('/usertasks') }}">我的求助</a></li>--}}
+                    {{--<li><a href="{{ url('/receivetasks') }}">我的帮忙</a></li>--}}
+                    {{--<li><a href="{{ url('/profile'.'/'.Auth::user()->id) }}">我的档案</a></li>--}}
+                {{--</ul>--}}
+            {{--@endif--}}
+        {{--</div>--}}
         {{--<div id="navbar" class="navbar-collapse collapse">--}}
             {{--<ul class="nav navbar-nav">--}}
                 {{--<li><a href="#about">About</a></li>--}}
@@ -43,24 +43,67 @@
                 {{--<li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>--}}
             {{--</ul>--}}
 
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">登录</a></li>
-                    <li><a href="{{ url('/register') }}">注册</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+            {{--<!-- Right Side Of Navbar -->--}}
+            {{--<ul class="nav navbar-nav navbar-right">--}}
+                {{--<!-- Authentication Links -->--}}
+                {{--@if (Auth::guest())--}}
+                    {{--<li><a href="{{ url('/login') }}">登录</a></li>--}}
+                    {{--<li><a href="{{ url('/register') }}">注册</a></li>--}}
+                {{--@else--}}
+                    {{--<li class="dropdown">--}}
+                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+                            {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
+                        {{--</a>--}}
 
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}">登出</a></li>
-                        </ul>
-                    </li>
-                @endif
+                        {{--<ul class="dropdown-menu" role="menu">--}}
+                            {{--<li><a href="{{ url('/logout') }}">登出</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                {{--@endif--}}
+            {{--</ul>--}}
+        {{--</div><!--/.nav-collapse -->--}}
+    {{--</div>--}}
+{{--</nav>--}}
+
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid" id="navfluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./Index.html">好邻居</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navigationbar">
+            @if(Auth::check())
+            <ul class="nav navbar-nav">
+                <li><a href="{{ url('/timelines').'/'.Auth::user()->id }}">我的故事</a></li>
+                <li><a href="{{ url('/points/user') }}">我的积分</a></li>
+                <li><a href="{{ url('/usertasks') }}">我的求助</a></li>
+                <li><a href="{{ url('/receivetasks') }}">我的帮忙</a></li>
+                <li><a href="{{ url('/profile'.'/'.Auth::user()->id) }}">我的档案</a></li>
             </ul>
-        </div><!--/.nav-collapse -->
-    </div>
+            @endif
+
+            <ul class="nav navbar-nav navbar-right">
+            <!-- Authentication Links -->
+            @if (Auth::guest())
+                <li><a href="{{ url('/login') }}">登录</a></li>
+                <li><a href="{{ url('/register') }}">注册</a></li>
+            @else
+                <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="{{ url('/logout') }}">登出</a></li>
+                </ul>
+                 </li>
+            @endif
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
