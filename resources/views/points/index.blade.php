cc4f2095f5142e78eea584602a75828454b55c40
cliuxinxin <cliuxinxin@gmail.com>
2016-05-22 19:20:44 +0800

    point display toggle

--- resources/views/points/index.blade.php
+++ resources/views/points/index.blade.php
@@ -4,17 +4,33 @@
     <div class="container">
         <h1>{{ Auth::user()->name }}的积分</h1>
 
-        <div class="well well-lg col-md-12">
-            您的积分有：{{ Auth::user()->havePoints() }}
+        {{--<div class="panel panel-default well well-lg col-md-12 lead">--}}
+            {{--您的积分有：{{ Auth::user()->havePoints() }}--}}
+        {{--</div>--}}
+
+        <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading">拥有积分</div>
+            <!-- Table -->
+            <table class="table">
+                <thead>
+                <th>积分</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ Auth::user()->havePoints() }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
         <div class="panel panel-default">
             <!-- Default panel contents -->
-            <div class="panel-heading">积分来源</div>
+            <div class="panel-heading">积分收入</div>
             <!-- Table -->
             <table class="table">
                 <thead>
-                    <th>积分来源</th>
+                    <th>积分收入</th>
                     <th>积分</th>
                 </thead>
                 <tbody>
@@ -24,7 +40,7 @@
                                 @if( $get_point->details )
                                     {{ $get_point->details }}
                                     @else
-                                    任务：{{ $get_point->task->content }}
+                                    任务：<a href="{{ url('tasks').'/'.$task->id }}">{{ $get_point->task->content }}</a>
                                 @endif
                                 </td>
                             <td>{{ $get_point->points }}</td>
@@ -37,7 +53,7 @@
         @if(count($spend_points)>0)
         <div class="panel panel-default">
             <!-- Default panel contents -->
-            <div class="panel-heading">积分花费</div>
+            <div class="panel-heading">积分消费</div>
             <!-- Table -->
             <table class="table">
                 <thead>
@@ -47,7 +63,7 @@
                 <tbody>
                 @foreach($spend_points as $spend_point)
                     <tr>
-                        <td>{{ $spend_point->task->content }}</td>
+                        <td><a href="{{ url('tasks').'/'.$task->id }}">{{ $spend_point->task->content }}</a></td>
                         <td>{{ $spend_point->points }}</td>
                     </tr>
                 @endforeach
@@ -59,17 +75,17 @@
         @if(count($unconfirm_get_points)>0)
             <div class="panel panel-default">
                 <!-- Default panel contents -->
-                <div class="panel-heading">未确认积分</div>
+                <div class="panel-heading">未确认收入</div>
                 <!-- Table -->
                 <table class="table">
                     <thead>
-                    <th>积分任务</th>
+                    <th>任务</th>
                     <th>积分</th>
                     </thead>
                     <tbody>
                     @foreach($unconfirm_get_points as $unconfirm_get_point)
                         <tr>
-                            <td>{{ $unconfirm_get_point->task->content }}</td>
+                            <td><a href="{{ url('tasks').'/'.$task->id }}">{{ $unconfirm_get_point->task->content }}</a></td>
                             <td>{{ $unconfirm_get_point->points }}</td>
                         </tr>
                     @endforeach
@@ -81,17 +97,17 @@
         @if(count($unconfirm_spend_points)>0)
             <div class="panel panel-default">
                 <!-- Default panel contents -->
-                <div class="panel-heading">未确认花费积分</div>
+                <div class="panel-heading">未确认消费</div>
                 <!-- Table -->
                 <table class="table">
                     <thead>
-                    <th>积分任务</th>
+                    <th>任务</th>
                     <th>积分</th>
                     </thead>
                     <tbody>
                     @foreach($unconfirm_spend_points as $unconfirm_spend_point)
                         <tr>
-                            <td>{{ $unconfirm_spend_point->task->content }}</td>
+                            <td><a href="{{ url('tasks').'/'.$task->id }}">{{ $unconfirm_spend_point->task->content }}</a></td>
                             <td>{{ $unconfirm_spend_point->points }}</td>
                         </tr>
                     @endforeach
