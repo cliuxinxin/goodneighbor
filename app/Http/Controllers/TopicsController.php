3166fe9f43b5f9dd8d581dd4b158083e5857808d
cliuxinxin <cliuxinxin@gmail.com>
2016-12-07 21:04:34 +0800

    no duplicate

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -157,8 +157,7 @@ class TopicsController extends Controller
     public function test()
     {
 
-        $nodevalues = $this->getInfosByListId('343');
-        return $nodevalues;
+        return "ok";
     }
 
 
@@ -187,7 +186,7 @@ class TopicsController extends Controller
 
 //        $bible = Bible::find(rand(1,31102));
 //        $nce = Nce::find(rand(1,5865));
-        $infos = Info::latest()->paginate(20);
+        $infos = Info::groupBy('title')->latest()->paginate(20);
 
         return view('topics.index',compact('gaoqings','bit_coin_price','stock_price','infos'));
     }
